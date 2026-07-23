9a3069743f963c7e107011488f70de65f9046468
Sofia Sabrina <sofiasabrinasilva@gmail.com>
2026-07-22 21:49:48 -0300

    feat: cria seção Hero com título, descrição e botões de ação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/layout/Header'
+import Hero from './components/sections/Hero'
 
 function App(){
   return(
     <div>
       <Header />
+      <Hero />
     </div>
   )
 }
