0085cbd1cb220324e35031f737941f23b2b1b965
Sofia Sabrina <sofiasabrinasilva@gmail.com>
2026-07-23 00:21:55 -0300

    feat: cria seção Stats com ícones lucide-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/layout/Header'
 import Hero from './components/sections/Hero'
+import Stats from './components/sections/Stats'
 
 function App(){
   return(
     <div>
       <Header />
       <Hero />
+      <Stats />
     </div>
   )
 }
